d92e31460af109a2c90626dc4fa8e4923c2fd08d
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2016-06-10 11:10:44 +0200

    Configure Django static files

--- src/foo/settings.py
+++ src/foo/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
